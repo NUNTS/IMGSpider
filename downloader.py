3c73a949220ebf45b5944435f13929e862fb67f4
nunts <sharkrain.@outlook.com>
2018-03-09 23:15:22 +0800

    分两个类抓取图片

--- downloader.py
+++ downloader.py
@@ -3,10 +3,10 @@ import time
 import os
 
 
-class DownLoader:
+class DownLoader():
     def __init__(self, url_list, path='temp', ext_name='.jpg', sleep_time=1):
-        self.url_list = url_list,
-        self.path = path,
+        self.url_list = url_list
+        self.path = path + os.sep
         self.ext_name = ext_name
         self.s = requests.session()
         self.sleep_time = sleep_time
@@ -18,7 +18,7 @@ class DownLoader:
         else:
             os.mkdir(str(self.path))
         for url in self.url_list:
-            with open(''.format(self.path + count + self.ext_name), 'wb')as img:
+            with open(self.path + str(count) + self.ext_name, 'wb') as img:
                 img.write(self.s.get(url).content)
                 count += 1
                 time.sleep(1)
--- imgspider.py
+++ imgspider.py
@@ -23,22 +23,19 @@ class IMGSpider:
         obj_url = obj.findall(doc)
         # 缩略图
         for i in thumb_url.copy():
-            thumb_url[thumb_url.index(i)] = (i.replace('\\', '').replace('"thumbURL":"', ''))
+            thumb_url[thumb_url.index(i)] = (
+                i.replace('\\', '').replace('"thumbURL":"', ''))
         # 大图
         for i in obj_url.copy():
-            obj_url[obj_url.index(i)] = (i.replace('\\', '').replace('"objURL":"', ''))
+            obj_url[obj_url.index(i)] = (
+                i.replace('\\', '').replace('"objURL":"', ''))
         self.img_dict['thumb_url'] = thumb_url
         self.img_dict['obj_url'] = obj_url
         return self.img_dict
 
-    def sav_img_dict(self, key_word=''):
-        res = self.get_img_dict(key_word)
-        with open('temp.json', 'w', encoding='utf-8') as u:
-            json.dump(res, u)
-
 
 if __name__ == '__main__':
     img = IMGSpider()
-    print(img.get_img_dict('美食')['obj_url'])
-    d = d(url_list=img.get_img_dict('美食')['obj_url'])
+    list = img.get_img_dict('鲁迅')['thumb_url']
+    d = d(url_list=list)
     d.download()
